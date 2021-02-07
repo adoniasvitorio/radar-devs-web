ef0d25b23c9428745d575afd1418ad6ba4cdb717
Adonias Vitorio <adonias.vitorio@gmail.com>
2021-02-07 01:22:05 -0300

    initial content

--- src/App.js
+++ src/App.js
@@ -1,6 +1,41 @@
+import React from 'react';
+
+import './global.css';
+import './App.css';
+import './Sidebar.css';
+
 function App() {
   return (
-    <div className="App">
+    <div id="app">
+      <aside>
+        <strong>Cadastrar</strong>
+        <form>
+          <div className="input-block">
+            <label htmlFor="github_username">Usuario Github</label>
+            <input name="github_username" id="github_username" required />
+          </div>
+
+          <div className="input-block">
+            <label htmlFor="techs">Tecnologias</label>
+            <input name="techs" id="techs" required />
+          </div>
+
+          <div className="input-group">
+            <div className="input-block">
+              <label htmlFor="lat">Latitude</label>
+              <input name="lat" id="lat" required />
+            </div>
+
+            <div className="input-block">
+              <label htmlFor="long">Longitude</label>
+              <input name="long" id="long" required />
+            </div>
+          </div>
+
+          <button type="submit">Salvar</button>
+        </form>
+      </aside>
+      <main></main>
     </div>
   );
 }
